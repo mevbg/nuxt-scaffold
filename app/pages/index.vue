f06accc2431f53d9305d90e8b31650b587e1d404
Martin Metodiev <martin@mev.bg>
2025-08-21 14:22:00 +0300

    Define ColorSchemeController

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -2,17 +2,7 @@
   <NuxtLayout>
     <Text variant="display" class="text-center">{{ $t('LOCL_SITE_TITLE') }}</Text>
 
-    <ConditionalClientOnly :condition="!serverSideSystemScheme">
-      <div class="mt-300x400 flex flex-wrap justify-center gap-300x400">
-        <Button
-          v-for="(btn, index) in colorSchemeModes"
-          :key="index"
-          :variant="(!btn && !colorSchemeMode) || colorSchemeMode === btn ? 'solid' : 'outlined'"
-          :label="btn"
-          @click="setColorSchemeMode(btn)"
-        />
-      </div>
-    </ConditionalClientOnly>
+    <ColorSchemeController />
   </NuxtLayout>
 </template>
 
@@ -21,28 +11,4 @@ definePageMeta({
   name: 'home',
   layout: 'basic'
 });
-
-const {
-  availableModes: colorSchemeModes,
-  currentMode: colorSchemeMode,
-  serverSideSystemScheme,
-  setColorSchemeMode
-} = useColorScheme();
-
-// TODO: remove this
-// try {
-//   const { data: offer, error: offerError } = await useFetch(`/api/search/${5}`);
-
-//   if (offerError.value) {
-//     throw createError(offerError.value);
-//   }
-
-//   console.log(offer.value);
-// } catch (err) {
-//   throw createError(err);
-// }
 </script>
-
-<style module>
-/* .root {} */
-</style>
